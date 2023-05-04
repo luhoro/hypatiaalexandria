f1323e24b95e1b39035bd60051126be50fe28aa2
lushoro <luisashorowitz@gmail.com>
2023-05-04 09:54:53 -0300

    undo refactor carousel

--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,20 @@ const controls = document.querySelectorAll('.control')
 
 let currentItem = 0
 const items = document.querySelectorAll('.item')
-
 const maxItems = items.length
 
 controls.forEach( control => {
     control.addEventListener('click', () => {
         const isLeft = control.classList.contains('arrow-left')
         
+        //move Items
         if (isLeft) {
             currentItem -= 1
         } else {
             currentItem += 1
         }
 
+        //show Items
         if (currentItem >= maxItems) {
             currentItem = 0
         } else if (currentItem < 0) {
@@ -23,8 +24,12 @@ controls.forEach( control => {
 
         items.forEach(item => item.classList.remove('current-item'))
 
+        //scroll property
         items[currentItem].scrollIntoView({
-            inline: "center"
+            inline: "center",
+            behavior: "smooth"
         })
+
+        items[currentItem].classList.add("current-item")
     })
 })
