2003f28fd0091b2e5d79316dbc569c88a29fcbac
lushoro <luisashorowitz@gmail.com>
2023-05-04 09:33:54 -0300

    refactor carousel

--- assets/script.js
+++ assets/script.js
@@ -2,29 +2,40 @@ const controls = document.querySelectorAll('.control')
 
 let currentItem = 0
 const items = document.querySelectorAll('.item')
-
 const maxItems = items.length
 
 controls.forEach( control => {
     control.addEventListener('click', () => {
         const isLeft = control.classList.contains('arrow-left')
         
-        if (isLeft) {
-            currentItem -= 1
-        } else {
-            currentItem += 1
-        }
-
-        if (currentItem >= maxItems) {
-            currentItem = 0
-        } else if (currentItem < 0) {
-            currentItem = maxItems - 1
-        }
+        moveItems()
+        showItems()
 
         items.forEach(item => item.classList.remove('current-item'))
 
+        //scroll property
         items[currentItem].scrollIntoView({
-            inline: "center"
+            inline: "center",
+            behavior: "smooth"
         })
+
+        items[currentItem].classList.add("current-item")
     })
 })
+
+
+function moveItems() {
+    if (isLeft) {
+        currentItem -= 1
+    } else {
+        currentItem += 1
+    }
+}
+
+function showItems() {
+    if (currentItem >= maxItems) {
+        currentItem = 0
+    } else if (currentItem < 0) {
+        currentItem = maxItems - 1
+    }
+}
